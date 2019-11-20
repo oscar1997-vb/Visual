c1c840858be9e119528cc5c462a715e2c507f10f
leonardo-puerto <36306517+leonardo-puerto@users.noreply.github.com>
2019-11-20 10:05:23 -0500

    Python.v1
    Primer formulario Python completo
    leonardo-Puerto

--- evaluacion/Form_Phyton.Designer.vb
+++ evaluacion/Form_Phyton.Designer.vb
@@ -22,7 +22,29 @@ Partial Class Form_Phyton
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form_Phyton))
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.pbLinux = New System.Windows.Forms.PictureBox()
+        Me.pbWin = New System.Windows.Forms.PictureBox()
+        Me.pbMac = New System.Windows.Forms.PictureBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.btnPython1Siguiente = New System.Windows.Forms.Button()
+        Me.btnPython1Menu = New System.Windows.Forms.Button()
+        Me.btnPython1Salir = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pbLinux, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pbWin, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pbMac, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Label1
@@ -34,18 +56,221 @@ Partial Class Form_Phyton
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Leonardo Puerto Layton"
         '
+        'PictureBox1
+        '
+        Me.PictureBox1.ErrorImage = CType(resources.GetObject("PictureBox1.ErrorImage"), System.Drawing.Image)
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.InitialImage = CType(resources.GetObject("PictureBox1.InitialImage"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(688, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(100, 100)
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(343, 13)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(74, 24)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Python"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 80)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(675, 13)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "El proceso de instalación de Python depende de el SO que se maneja, en los siguie" &
+    "ntes enlaces se podrá ver una guía para hacer el proceso"
+        '
+        'pbLinux
+        '
+        Me.pbLinux.Image = CType(resources.GetObject("pbLinux.Image"), System.Drawing.Image)
+        Me.pbLinux.Location = New System.Drawing.Point(322, 113)
+        Me.pbLinux.Name = "pbLinux"
+        Me.pbLinux.Size = New System.Drawing.Size(100, 94)
+        Me.pbLinux.TabIndex = 4
+        Me.pbLinux.TabStop = False
+        '
+        'pbWin
+        '
+        Me.pbWin.Image = CType(resources.GetObject("pbWin.Image"), System.Drawing.Image)
+        Me.pbWin.Location = New System.Drawing.Point(162, 113)
+        Me.pbWin.Name = "pbWin"
+        Me.pbWin.Size = New System.Drawing.Size(100, 94)
+        Me.pbWin.TabIndex = 5
+        Me.pbWin.TabStop = False
+        '
+        'pbMac
+        '
+        Me.pbMac.Image = CType(resources.GetObject("pbMac.Image"), System.Drawing.Image)
+        Me.pbMac.Location = New System.Drawing.Point(486, 113)
+        Me.pbMac.Name = "pbMac"
+        Me.pbMac.Size = New System.Drawing.Size(82, 100)
+        Me.pbMac.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.pbMac.TabIndex = 6
+        Me.pbMac.TabStop = False
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(11, 56)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(98, 24)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Instalación"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(102, 235)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(144, 24)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "Verificar versión"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(15, 275)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(360, 39)
+        Me.Label6.TabIndex = 8
+        Me.Label6.Text = "Para reconocer la versión de Python que se está usando se debe poner el " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "comando" &
+    ": python -V, " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "veamos la salida en un Linux"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Courier New", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(103, 325)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(210, 28)
+        Me.Label7.TabIndex = 10
+        Me.Label7.Text = "[root@localhost ~]# python -V" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Python 2.7.5"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(14, 372)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(340, 26)
+        Me.Label8.TabIndex = 11
+        Me.Label8.Text = "Reconocer la versión de Python permitirá al programador establecer el " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "alcance q" &
+    "ue tiene dependiendo de la misma."
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Font = New System.Drawing.Font("Courier New", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label10.Location = New System.Drawing.Point(471, 372)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(280, 28)
+        Me.Label10.TabIndex = 14
+        Me.Label10.Text = "[root@localhost ~]# python holamundo.py" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Hola mundo"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label11.Location = New System.Drawing.Point(531, 235)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(114, 24)
+        Me.Label11.TabIndex = 13
+        Me.Label11.Text = "Hola Mundo"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(428, 275)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(363, 91)
+        Me.Label12.TabIndex = 12
+        Me.Label12.Text = resources.GetString("Label12.Text")
+        '
+        'btnPython1Siguiente
+        '
+        Me.btnPython1Siguiente.Location = New System.Drawing.Point(360, 418)
+        Me.btnPython1Siguiente.Name = "btnPython1Siguiente"
+        Me.btnPython1Siguiente.Size = New System.Drawing.Size(75, 23)
+        Me.btnPython1Siguiente.TabIndex = 15
+        Me.btnPython1Siguiente.Text = "Siguiente"
+        Me.btnPython1Siguiente.UseVisualStyleBackColor = True
+        '
+        'btnPython1Menu
+        '
+        Me.btnPython1Menu.Location = New System.Drawing.Point(279, 418)
+        Me.btnPython1Menu.Name = "btnPython1Menu"
+        Me.btnPython1Menu.Size = New System.Drawing.Size(75, 23)
+        Me.btnPython1Menu.TabIndex = 16
+        Me.btnPython1Menu.Text = "Menú"
+        Me.btnPython1Menu.UseVisualStyleBackColor = True
+        '
+        'btnPython1Salir
+        '
+        Me.btnPython1Salir.Location = New System.Drawing.Point(442, 418)
+        Me.btnPython1Salir.Name = "btnPython1Salir"
+        Me.btnPython1Salir.Size = New System.Drawing.Size(75, 23)
+        Me.btnPython1Salir.TabIndex = 17
+        Me.btnPython1Salir.Text = "Salir"
+        Me.btnPython1Salir.UseVisualStyleBackColor = True
+        '
         'Form_Phyton
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnPython1Salir)
+        Me.Controls.Add(Me.btnPython1Menu)
+        Me.Controls.Add(Me.btnPython1Siguiente)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.pbMac)
+        Me.Controls.Add(Me.pbWin)
+        Me.Controls.Add(Me.pbLinux)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.Label1)
         Me.Name = "Form_Phyton"
         Me.Text = "Ayudas Académicas Phyton"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pbLinux, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pbWin, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pbMac, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents Label1 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents pbLinux As PictureBox
+    Friend WithEvents pbWin As PictureBox
+    Friend WithEvents pbMac As PictureBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents btnPython1Siguiente As Button
+    Friend WithEvents btnPython1Menu As Button
+    Friend WithEvents btnPython1Salir As Button
 End Class
--- evaluacion/Form_Phyton.vb
+++ evaluacion/Form_Phyton.vb
@@ -1,3 +1,29 @@
 ﻿Public Class Form_Phyton
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles pbLinux.Click
+        Process.Start("https://entrenamiento-python-basico.readthedocs.io/es/latest/leccion1/instalacion.html#instalando-python-en-un-linux")
+    End Sub
 
+    Private Sub PbWin_Click(sender As Object, e As EventArgs) Handles pbWin.Click
+        Process.Start("https://www.youtube.com/watch?v=VTykmP-a2KY")
+    End Sub
+
+    Private Sub PbMac_Click(sender As Object, e As EventArgs) Handles pbMac.Click
+        Process.Start("https://es.wikibooks.org/wiki/Python/Instalaci%C3%B3n_de_Python/Python_en_Mac_OS_X")
+    End Sub
+
+    Private Sub BtnPython1_Click(sender As Object, e As EventArgs) Handles btnPython1Siguiente.Click
+        Me.Hide()
+        Form_Phyton2.Show()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnPython1Menu.Click
+        Me.Close()
+        Form_menu.Show()
+    End Sub
+
+    Private Sub BtnPython1Salir_Click(sender As Object, e As EventArgs) Handles btnPython1Salir.Click
+        Me.Close()
+        Form_menu.Close()
+        Form_Phyton2.Close()
+    End Sub
 End Class
